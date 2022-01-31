54ddef08acead5e319f00e4a968a137000470438
Yurii Prykhodko <yurii.prykhodko@solid.software>
2022-01-31 11:38:21 +0200

    Add solid_lints

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:three_days/src/template_app.dart';
 
 void main() {
-  runApp(const TemplateApp());
-}
-
-class TemplateApp extends StatelessWidget {
-  const TemplateApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Template app',
-      home: Container(),
-    );
-  }
+  runApp(TemplateApp());
 }
---
+++ lib/src/template_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/// This is a template application.
+/// 
+/// Feel free to replace this with your custom code.
+class TemplateApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Template app',
+      home: Container(),
+    );
+  }
+}
