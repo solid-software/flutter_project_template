a406173ef43cc90cf76929a2c342b8470fb14354
Yurii Prykhodko <yurii.prykhodko@solid.software>
2022-01-31 14:33:48 +0200

    Remove Flutter template

--- android/app/src/main/kotlin/com/example/three_days/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.three_days
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:project_template/template_app.dart';
-
-void main() {
-  runApp(TemplateApp());
-}
